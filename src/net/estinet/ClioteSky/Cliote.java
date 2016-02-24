ba483bf6073146b5d8678c4a58f4816e2be108fe
_InDev_ <aguy867@gmail.com>
2016-02-24 18:53:39 -0500

    Changes

---
+++ src/net/estinet/ClioteSky/Cliote.java
@@ -0,0 +1,6 @@
+package net.estinet.ClioteSky;
+
+public class Cliote {
+	private String name = "";
+	private String ip = "127.0.0.1";
+}
---
+++ src/net/estinet/ClioteSky/configuration/Config.java
@@ -0,0 +1,8 @@
+package net.estinet.ClioteSky.configuration;
+
+public class Config {
+	public void setConfig(){
+		File f = new File("./Data");
+		File file = new File("./Data/config.dat");
+	}
+}
