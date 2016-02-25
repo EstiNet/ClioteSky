9a7d20d48d48207ecc9e451371280c4e124db846
_InDev_ <aguy867@gmail.com>
2016-02-24 19:00:24 -0500

    RSA

--- src/net/estinet/ClioteSky/Cliote.java
+++ src/net/estinet/ClioteSky/Cliote.java
@@ -3,4 +3,20 @@ package net.estinet.ClioteSky;
 public class Cliote {
 	private String name = "";
 	private String ip = "127.0.0.1";
+	public Cliote(String connectionname, String connectip){
+		name = connectionname;
+		ip = connectip;
+	}
+	public String getName(){
+		return name;
+	}
+	public String getIP(){
+		return ip;
+	}
+	public void setName(String newname){
+		name = newname;
+	}
+	public void setIP(String newip){
+		ip = newip;
+	}
 }
--- src/net/estinet/ClioteSky/configuration/Config.java
+++ src/net/estinet/ClioteSky/configuration/Config.java
@@ -1,8 +1,41 @@
 package net.estinet.ClioteSky.configuration;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Config {
 	public void setConfig(){
 		File f = new File("./Data");
 		File file = new File("./Data/config.dat");
+		File rsa = new File("./RSA");
+		File rsapub = new File("./RSA/public.key");
+		File rsapri = new File("./RSA/private.key");
+		if(!f.isDirectory()){
+			f.mkdir();
+		}
+		if(!rsa.isDirectory()){
+			rsa.mkdir();
+		}
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if(!rsapub.exists()){
+			try {
+				rsapub.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if(!rsapri.exists()){
+			try {
+				rsapri.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
