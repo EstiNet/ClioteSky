eb6a2c7d109598c83031cac71f7cf6585d1d78c8
_InDev_ <aguy867@gmail.com>
2016-02-24 20:23:08 -0500

    Changes

--- src/net/estinet/ClioteSky/CommandSystem.java
+++ src/net/estinet/ClioteSky/CommandSystem.java
@@ -1,8 +1,13 @@
 package net.estinet.ClioteSky;
 
+import java.util.Scanner;
+
 public class CommandSystem {
 	protected void start(){
 		while(ClioteSky.exit){
+			Scanner scan = new Scanner(System.in);
+			String input = scan.nextLine();
+			scan.close();
 			
 		}
 	}
