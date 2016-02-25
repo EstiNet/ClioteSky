f3b86e7f1008be07e19ef8b352c471b3d76201b5
_InDev_ <aguy867@gmail.com>
2016-02-25 17:32:52 -0500

    Changes

--- src/net/estinet/ClioteSky/CommandSystem.java
+++ src/net/estinet/ClioteSky/CommandSystem.java
@@ -5,37 +5,34 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CommandSystem {
+	Scanner scan = new Scanner(System.in);
 	protected void start(){
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			System.out.print("> ");
-			Scanner scan = new Scanner(System.in);
+			System.out.print("\n> ");
 			String input = scan.nextLine();
-			scan.close();
 			String[] args = input.split("\\s+");
 			List<String> argss = new ArrayList<>();	
 			for(String str : args){
 				argss.add(str);
 			}
+			boolean lel = true;
 			for(Command command : ClioteSky.commands){
 				if(command.getName().equalsIgnoreCase(args[0])){
+					lel = false;
 					Thread thr = new Thread(new Runnable(){
 						public void run(){
 							command.run(argss);
 						}
 					});
 					thr.start();
-					if(thr.isAlive()){
+					if(!thr.isAlive()){
 						start();
+						break;
 					}
 					else{
 						Thread thr2 = new Thread(new Runnable(){
 							public void run(){
 								while(true){
-									if(thr.isAlive()){
+									if(!thr.isAlive()){
 										start();
 										break;
 									}
@@ -53,6 +50,9 @@ public class CommandSystem {
 					}
 				}
 			}
-			System.out.println("\n[System]: Command not recognized. :P");
+			if(lel){
+				System.out.println("\n[System]: Command not recognized. :P");
+				start();
+			}
 	}
 }
---
+++ src/net/estinet/ClioteSky/Disable.java
@@ -0,0 +1,8 @@
+package net.estinet.ClioteSky;
+
+public class Disable {
+	public void stop(){
+		ClioteSky.println("Stopping ClioteSky version " + ClioteSky.version + "...");
+		System.exit(0);
+	}
+}
