154db4b2f89625906e3e95c403bdecdb3ad504b4
_InDev_ <aguy867@gmail.com>
2016-03-18 17:44:46 -0400

    Changes

--- src/net/estinet/ClioteSky/network/protocol/input/InputChange.java
+++ src/net/estinet/ClioteSky/network/protocol/input/InputChange.java
@@ -1,10 +1,16 @@
 package net.estinet.ClioteSky.network.protocol.input;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+import net.estinet.ClioteSky.Category;
 import net.estinet.ClioteSky.Cliote;
+import net.estinet.ClioteSky.ClioteSky;
+import net.estinet.ClioteSky.exceptions.CreationCategoryNotKnownException300;
 import net.estinet.ClioteSky.exceptions.IncorrectArgumentsException101;
+import net.estinet.ClioteSky.exceptions.NameNotKnownException201;
+import net.estinet.ClioteSky.exceptions.PasswordIncorrectException900;
 import net.estinet.ClioteSky.exceptions.RegisterFirstException901;
 import net.estinet.ClioteSky.network.protocol.InputPacket;
 import net.estinet.ClioteSky.network.protocol.Packet;
@@ -42,7 +48,68 @@ public class InputChange extends InputPacket implements Packet {
 				}
 			}
 			else{
-				
+				String clioteName = args.get(0), category = args.get(1), password = args.get(2);
+				if(ClioteSky.getCliote(clioteName) == null){
+					try{
+						throw new NameNotKnownException201();
+					}
+					catch(NameNotKnownException201 e){
+						e.printStackTrace();
+						OutputError oe = new OutputError();
+						oe.run(Arrays.asList("201"), sender);
+					}
+				}
+				else{
+					boolean bool = false;
+					for(Category categorys : ClioteSky.categories){
+						if(categorys.getName().equals(category)){
+							bool = false;
+						}
+					}
+					if(bool){
+						if(!ClioteSky.getCliote(clioteName).getPassword().equals(password)){
+							try{
+								throw new PasswordIncorrectException900();
+							}
+							catch(PasswordIncorrectException900 e){
+								e.printStackTrace();
+								OutputError oe = new OutputError();
+								oe.run(Arrays.asList("900"), sender);
+							}
+						}
+						else{
+							for(int i = 0; i < ClioteSky.categories.size(); i++){
+								for(int it = 0; it < ClioteSky.categories.get(i).getCliotes().size(); it++){
+									if(ClioteSky.categories.get(i).getCliotes().get(it).getName().equals(clioteName)){
+										List<Cliote> cliotes = ClioteSky.categories.get(i).getCliotes();
+										Cliote cliote = ClioteSky.categories.get(i).getCliotes().get(it);
+										cliotes.remove(it);
+										ClioteSky.categories.get(i).setCliotes(cliotes);
+										File getRidOf = new File("./Cliotes/" + ClioteSky.categories.get(i).getName() + "/" + cliote.getName());
+										getRidOf.delete();
+										for(int iterator = 0; iterator < ClioteSky.categories.size(); iterator++){
+											if(ClioteSky.categories.get(iterator).getName().equals(category)){
+												ClioteSky.categories.get(iterator).addCliote(cliote);
+												ClioteSky.println("Sucessfully changed categories for Cliote " + cliote.getName());
+											}
+										}
+										break;
+									}
+								}
+							}
+						}
+					}
+					else{
+						try{
+							throw new CreationCategoryNotKnownException300();
+						}
+						catch(CreationCategoryNotKnownException300 e){
+							e.printStackTrace();
+							OutputError oe = new OutputError();
+							oe.run(Arrays.asList("300"), sender);
+						}
+					}
+				}
 			}
 		}
 	}
